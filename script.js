125dca662b81d80f087f42b2a2f9c9bcee4039d7
pbvhio <alecmaicy@gmail.com>
2023-06-18 19:05:10 +0800

    init

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("about-me").addEventListener("click", function() {
+  document.getElementById("about-me-image").style.display = "block";
+  console.log(document.getElementById("about-me-image").style)
+});
